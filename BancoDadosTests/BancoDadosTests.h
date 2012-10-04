0d7e99dc35affbeadaf17c4658bf145e25c7f633
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-10-04 09:31:20 -0300

    Initial Commit

---
+++ BancoDadosTests/BancoDadosTests.h
@@ -0,0 +1,13 @@
+//
+//  BancoDadosTests.h
+//  BancoDadosTests
+//
+//  Created by Rafael Brigagão Paulino on 04/10/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BancoDadosTests : SenTestCase
+
+@end
---
+++ BancoDadosTests/BancoDadosTests.m
@@ -0,0 +1,32 @@
+//
+//  BancoDadosTests.m
+//  BancoDadosTests
+//
+//  Created by Rafael Brigagão Paulino on 04/10/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "BancoDadosTests.h"
+
+@implementation BancoDadosTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BancoDadosTests");
+}
+
+@end
